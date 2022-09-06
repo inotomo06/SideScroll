81e29e1d41b83ed23b2d661de01147e76ab54d3b
inotomo06 <86100981+inotomo06@users.noreply.github.com>
2022-09-06 18:16:44 +0900

    Update : systemNameの変更

--- SideScroll/ContentView.swift
+++ SideScroll/ContentView.swift
@@ -9,35 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     // Tab を選ぶときの識別番号
-    @State private var select = 0
+    @State private var number = 0
     
     var body: some View {
         
         HStack {
-            Image(systemName: "control")
+            Image(systemName: "chevron.left")
                 .font(.largeTitle)
                 .padding()
-                .rotationEffect(Angle.degrees(270))  // 回転角度の指定
+//                .rotationEffect(Angle.degrees(270))  // 回転角度の指定
                 .onTapGesture {
-                    if select != 0 {
-                        select -= 1
+                    if number != 0 {
+                        number -= 1
                     }
                 }
             
             VStack {
-                TabView(selection: $select) {
-                    Text("View 01").background(Color.red).tag(0)
-                    Text("View 02").background(Color.blue).tag(1)
-                    Text("View 03").background(Color.orange).tag(2)
+                TabView(selection: $number) {
+                    Text("View 01").background(Color.red)
+                        .tag(0)
+                    Text("View 02").background(Color.blue)
+                        .tag(1)
+                    Text("View 03").background(Color.orange)
+                        .tag(2)
                 }
                 .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
                 .transition(.slide)
                 .animation(.easeInOut)
                 
-                // PageTabViewにすると切り替え用のバーが消えるためボタンで代用
+                // PageTabViewにすると切り替え用のバーが消えるためボタンを使う
                 HStack {
                     Button {
-                        select = 0
+                        number = 0
                     } label: {
                         Text("赤のViewへ")
                     }
@@ -48,7 +51,7 @@ struct ContentView: View {
                     .foregroundColor(.black)
                     
                     Button {
-                        select = 1
+                        number = 1
                     } label: {
                         Text("青のViewへ")
                     }
@@ -59,7 +62,7 @@ struct ContentView: View {
                     .foregroundColor(.black)
                     
                     Button {
-                        select = 2
+                        number = 2
                     } label: {
                         Text("黄のViewへ")
                     }
@@ -71,13 +74,13 @@ struct ContentView: View {
                 }
             }
             
-            Image(systemName: "control")
+            Image(systemName: "chevron.right")
                 .font(.largeTitle)
                 .padding()
-                .rotationEffect(Angle.degrees(90)) // 回転角度の指定
+//                .rotationEffect(Angle.degrees(90)) // 回転角度の指定
                 .onTapGesture {
-                    if select != 2 {
-                        select += 1
+                    if number != 2 {
+                        number += 1
                     }
                 }
         }
