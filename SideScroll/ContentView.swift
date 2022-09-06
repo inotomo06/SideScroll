3377eba4b34c1f124cd0518fab987a7293236952
inotomo06 <86100981+inotomo06@users.noreply.github.com>
2022-09-06 11:28:57 +0900

    Initial Commit

---
+++ SideScroll/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SideScroll
+//
+//  Created by cmStudent on 2022/09/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SideScroll/SideScrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideScrollApp.swift
+//  SideScroll
+//
+//  Created by cmStudent on 2022/09/06.
+//
+
+import SwiftUI
+
+@main
+struct SideScrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
