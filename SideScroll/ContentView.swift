55603d2527ab82aea1e61cb85d9b5e0bfd31e930
inotomo06 <86100981+inotomo06@users.noreply.github.com>
2022-09-06 12:58:56 +0900

    First Commit

--- SideScroll/ContentView.swift
+++ SideScroll/ContentView.swift
@@ -8,9 +8,79 @@
 import SwiftUI
 
 struct ContentView: View {
+    // Tab を選ぶときの識別番号
+    @State private var select = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        HStack {
+            Image(systemName: "control")
+                .font(.largeTitle)
+                .padding()
+                .rotationEffect(Angle.degrees(270))  // 回転角度の指定
+                .onTapGesture {
+                    if select != 0 {
+                        select -= 1
+                    }
+                }
+            
+            VStack {
+                TabView(selection: $select) {
+                    Text("View 01").background(Color.red).tag(0)
+                    Text("View 02").background(Color.blue).tag(1)
+                    Text("View 03").background(Color.orange).tag(2)
+                }
+                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+                .transition(.slide)
+                .animation(.easeInOut)
+                
+                // PageTabViewにすると切り替え用のバーが消えるためボタンで代用
+                HStack {
+                    Button {
+                        select = 0
+                    } label: {
+                        Text("赤のViewへ")
+                    }
+                    .padding()
+                    .lineLimit(1)
+                    .minimumScaleFactor(0.5)
+                    .background(Color.red)
+                    .foregroundColor(.black)
+                    
+                    Button {
+                        select = 1
+                    } label: {
+                        Text("青のViewへ")
+                    }
+                    .padding()
+                    .lineLimit(1)
+                    .minimumScaleFactor(0.5)
+                    .background(Color.blue)
+                    .foregroundColor(.black)
+                    
+                    Button {
+                        select = 2
+                    } label: {
+                        Text("黄のViewへ")
+                    }
+                    .padding()
+                    .lineLimit(1)
+                    .minimumScaleFactor(0.5)
+                    .background(Color.orange)
+                    .foregroundColor(.black)
+                }
+            }
+            
+            Image(systemName: "control")
+                .font(.largeTitle)
+                .padding()
+                .rotationEffect(Angle.degrees(90)) // 回転角度の指定
+                .onTapGesture {
+                    if select != 2 {
+                        select += 1
+                    }
+                }
+        }
     }
 }
 
